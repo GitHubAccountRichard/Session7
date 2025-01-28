99003c9cfceee6355b4f9bb65d5b9911ff369616
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-28 16:46:49 +0100

    added stuff about strings

---
+++ simple_strings.py
@@ -0,0 +1,14 @@
+s1 = "John" #doubel quote
+s2 = 'Mary' #single quote
+
+print(s1)
+print(s2)
+
+s1 = "and then he asked: 'how are you?'"
+print(s1)
+s1 = 'and then he asked: "how are you?"'
+print(s1)
+s1 = "This is how to have  \" and  \' inside a string"
+print(s1)
+
+
